d43127127d36a809a482f9a781daa24f5aa7d3d9
Godswill <godswilloliver@57.com>
2026-07-21 15:10:49 +0100

    made change in my code structure

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
-from turtle import update
-
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from dotenv import load_dotenv
 from telegram.ext import MessageHandler, filters
-# import update
+from database import create_table, save_message
 import os
 
 load_dotenv()
@@ -17,17 +15,20 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
-
-
 async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        telegram_id = update.message.from_user.id
         user_message = update.message.text
-        if user_message.upper() == "Hello" or user_message.lower() == "hello":
-          await update.message.reply_text("I'm doing great! How about you?")
-        elif user_message.upper() == "How was your day?":
+        save_message(telegram_id, user_message)
+        if user_message.lower() == "hello":
+            await update.message.reply_text("I'm doing great! How about you?")
+        elif user_message.lower() == "i'm fine, how about you?":
+            await update.message.reply_text("I'm glad to hear that! I'm doing well too.")
+        elif user_message.lower() == "how was your day?":
             await update.message.reply_text("It was good, thanks for asking!")
-        else:
-            await update.message.reply_text("I'm sorry, I didn't understand that. Can you please rephrase?")
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
 
 
+
+create_table()
 app.run_polling()
+
