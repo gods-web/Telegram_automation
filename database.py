0acb46b9df3137244acc3073a0662c9a64c104d9
Godswill <godswilloliver@57.com>
2026-07-29 14:45:24 +0100

    update was made to the database so that gemini can save and also know who is sending the message

--- database.py
+++ database.py
@@ -5,21 +5,24 @@ def create_table():
     cursor = conn.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS messages
                       (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                       telegram_id INTEGER,
-                       message TEXT)''')
+                       telegram_id INTEGER,message_from TEXT,message_type TEXT,file_id TEXT,created_at TIMESTAMP,message TEXT)''')
     conn.commit()
     conn.close()
 
 
-
-def save_message( telegram_id, user_message):
+def save_message( telegram_id, message_from, user_message):
     conn = sqlite3.connect('messages.db')
     cursor = conn.cursor()
     
-    cursor.execute('INSERT INTO messages (telegram_id, message) VALUES (?, ?)', (telegram_id, user_message))
+    cursor.execute('INSERT INTO messages (telegram_id,message_from, message) VALUES (?, ?, ?)', (telegram_id,message_from, user_message))
     conn.commit()
     conn.close()
 
+def get_chat_history(telegram_id):
+    conn = sqlite3.connect("messages.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM messages WHERE telegram_id = ? ORDER BY created_at DESC LIMIT 10", (telegram_id,))
+    chat_history = cursor.fetchall()
+    conn.close()
 
-
-
+    return chat_history
