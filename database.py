94bf5e38bb39c239075c9e9563e4929e6ceef860
Godswill <godswilloliver@57.com>
2026-07-30 02:43:23 +0100

    update data base

--- database.py
+++ database.py
@@ -1,20 +1,38 @@
 import sqlite3
 
 def create_table():
-    conn = sqlite3.connect('messages.db')
+    conn = sqlite3.connect("messages.db")
     cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS messages
-                      (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                       telegram_id INTEGER,message_from TEXT,message_type TEXT,file_id TEXT,created_at TIMESTAMP,message TEXT)''')
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS messages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            telegram_id INTEGER,
+            message_from TEXT,
+            message_type TEXT,
+            file_id TEXT,
+            file_path TEXT,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            message TEXT
+        )
+    """)
+
     conn.commit()
     conn.close()
 
 
-def save_message( telegram_id, message_from, user_message):
+def save_message(
+    telegram_id="telegram_id",
+    message_from="message_from",
+    message_type="text",
+    file_id=None,
+    file_path=None,
+    message="user_message"
+):
     conn = sqlite3.connect('messages.db')
     cursor = conn.cursor()
     
-    cursor.execute('INSERT INTO messages (telegram_id,message_from, message) VALUES (?, ?, ?)', (telegram_id,message_from, user_message))
+    cursor.execute('INSERT INTO messages (telegram_id,message_from, message_type, file_id, file_path, message) VALUES (?, ?, ?, ?, ?, ?)', (telegram_id,message_from, message_type, file_id, file_path, message))
     conn.commit()
     conn.close()
 
