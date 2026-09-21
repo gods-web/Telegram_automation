ee9e46bba455c376b6a3b5aef3a22aadebf27710
Godswill <godswilloliver@57.com>
2026-09-21 15:33:19 +0100

    Use Supabase database URL for Render

--- database.py
+++ database.py
@@ -7,12 +7,7 @@ load_dotenv()
 def get_connection():
 
     return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        port=os.getenv("DB_PORT"),
-        sslmode = "require"
+        os.getenv("DATABASE_URL")
     )
 
 
